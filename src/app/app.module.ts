3e9781775d888d5275758c2fc786ad0e5056a242
AlbitaMari <albasaenz89@gmail.com>
2022-07-23 02:21:46 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserdetailComponent } from './components/userdetail/userdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserdetailComponent
   ],
   imports: [
     BrowserModule,
